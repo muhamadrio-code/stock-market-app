e43ab978b8e7c5eaceb0a02a978f01389b1a32f7
muhammad rio <muhammad.rioperm@gmail.com>
2023-05-18 17:15:42 +0700

    Use hilt version version from root project instead of hard code it

--- build-logic/convention/src/main/java/AndroidHiltConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidHiltConventionPlugin.kt
@@ -1,5 +1,6 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.internal.extensibility.DefaultExtraPropertiesExtension
 import org.gradle.kotlin.dsl.dependencies
 
 class AndroidHiltConventionPlugin : Plugin<Project> {
@@ -10,9 +11,12 @@ class AndroidHiltConventionPlugin : Plugin<Project> {
                 apply("org.jetbrains.kotlin.kapt")
             }
 
+            val ext = rootProject.extensions.getByName("ext") as DefaultExtraPropertiesExtension
+            val hiltVersion = ext["hilt_version"]
+
             dependencies {
-                add("implementation", "com.google.dagger:hilt-android:2.44")
-                add("kapt", "com.google.dagger:hilt-android-compiler:2.44")
+                add("implementation", "com.google.dagger:hilt-android:$hiltVersion")
+                add("kapt", "com.google.dagger:hilt-android-compiler:$hiltVersion")
             }
         }
     }
