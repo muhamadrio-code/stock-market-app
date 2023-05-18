7dac63c305ff200b53772f909e34c133681cb055
muhammad rio <muhammad.rioperm@gmail.com>
2023-05-18 10:18:20 +0700

    Migrate hilt dependencies into separated gradle plugin [AndroidHiltConventionPlugin.kt]

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.riopermana.convention"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    compileOnly("com.android.tools.build:gradle:8.0.0")
+    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+}
+
+gradlePlugin {
+    plugins {
+        register("androidHilt") {
+            id = "riopermana.stockmarket.hilt"
+            implementationClass = "AndroidHiltConventionPlugin"
+        }
+    }
+    plugins {
+        register("androidApplication") {
+            id = "riopermana.stockmarket.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+    }
+}
