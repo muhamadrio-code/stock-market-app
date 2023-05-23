f1e85f97afb17810ea7e5cc4b8fa957ce772819c
muhammad rio <muhammad.rioperm@gmail.com>
2023-05-23 13:56:50 +0700

    Setup company listings repository injection

---
+++ core/data/src/main/java/di/DataModule.kt
@@ -0,0 +1,15 @@
+package di
+
+import com.riopermana.data.repository.CompanyListingsRepository
+import com.riopermana.data.repository.contract.ICompanyListingsRepository
+import dagger.Binds
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+interface DataModule {
+    @Binds
+    fun bindsCompanyListingRepository(
+        companyListingsRepository: CompanyListingsRepository
+    ) : ICompanyListingsRepository
+}
