bde0726eaaedb26ca084902f37323c0555823c94
muhammad rio <muhammad.rioperm@gmail.com>
2023-06-03 11:46:40 +0700

    Improve FakeCompanyListingsDao

--- core/data/src/test/java/com/riopermana/data/repository/fake/FakeCompanyListingsDao.kt
+++ core/data/src/test/java/com/riopermana/data/repository/fake/FakeCompanyListingsDao.kt
@@ -3,24 +3,12 @@ package com.riopermana.data.repository.fake
 import com.riopermana.database.CompanyListingDao
 import com.riopermana.database.entities.CompanyListingEntity
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flowOf
 
 class FakeCompanyListingsDao : CompanyListingDao {
-    private val companyListings = mutableSetOf(
-        CompanyListingEntity(1, "A", "Agilent Technologies Inc", "NYSE"),
-        CompanyListingEntity(2, "AA", "Alcoa Corp", "NYSE"),
-        CompanyListingEntity(3, "AAA", "AAX FIRST PRIORITY CLO BOND ETF", "NYSE ARCA"),
-        CompanyListingEntity(4, "AEAE", "AltEnergy Acquisition Corp - Class A", "NASDAQ"),
-        CompanyListingEntity(5, "ADXN", "Addex Therapeutics Ltd", "NASDAQ"),
-        CompanyListingEntity(6, "AA", "Aether Ltd", "NASDAQ"),
-    )
+    private val companyListings = mutableSetOf<CompanyListingEntity>()
 
-    override suspend fun clearCompanyListings() {
-        companyListings.clear()
-    }
-
-    override suspend fun insertCompanyListingsOrAbort(list: List<CompanyListingEntity>) {
+    override suspend fun insertOrIgnoreCompanyListings(list: List<CompanyListingEntity>) {
         companyListings.addAll(list)
     }
 
@@ -29,6 +17,12 @@ class FakeCompanyListingsDao : CompanyListingDao {
     }
 
     override fun getCompanyListings(query: String): Flow<List<CompanyListingEntity>> {
-        return emptyFlow()
+        return flowOf(
+            companyListings.filter { entity ->
+                entity.name.lowercase().contains(
+                    query.lowercase().toRegex()
+                ) || entity.symbol == query.uppercase()
+            }
+        )
     }
 }
