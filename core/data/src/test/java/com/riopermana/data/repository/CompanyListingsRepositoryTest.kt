7563500b711f7cfc4c780cae0cc52475963fc12d
muhammad rio <muhammad.rioperm@gmail.com>
2023-06-03 11:30:42 +0700

    Improve unit test for CompanyListingsRepository

--- core/data/src/test/java/com/riopermana/data/repository/CompanyListingsRepositoryTest.kt
+++ core/data/src/test/java/com/riopermana/data/repository/CompanyListingsRepositoryTest.kt
@@ -1,20 +1,20 @@
 package com.riopermana.data.repository
 
+import com.google.common.truth.Truth.*
 import com.riopermana.data.Synchronizer
+import com.riopermana.data.mapper.toListExternalModel
 import com.riopermana.data.model.CompanyListings
 import com.riopermana.data.repository.fake.FakeCompanyListingsDao
-import com.riopermana.database.CompanyListingDao
 import com.riopermana.database.entities.CompanyListingEntity
 import com.riopermana.network.datasource.RemoteDataSource
 import com.riopermana.network.dto.CompanyListingDto
 import io.mockk.coEvery
 import io.mockk.coVerify
-import io.mockk.every
 import io.mockk.mockk
+import io.mockk.spyk
 import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
@@ -26,85 +26,73 @@ import org.junit.Test
 class CompanyListingsRepositoryTest {
 
     private lateinit var mockRemoteDataSource: RemoteDataSource
-    private lateinit var mockDao: CompanyListingDao
+    private lateinit var companyListingsDao: FakeCompanyListingsDao
+    private lateinit var spyDao: FakeCompanyListingsDao
     private lateinit var repository: OfflineFirstCompanyListingsRepository
     private val testScope = TestScope(UnconfinedTestDispatcher())
 
     @Before
     fun setUp() {
         mockRemoteDataSource = mockk()
-        mockDao = mockk()
-        repository = OfflineFirstCompanyListingsRepository(mockRemoteDataSource, mockDao)
+        companyListingsDao = FakeCompanyListingsDao()
+        spyDao = spyk(companyListingsDao)
+        repository = OfflineFirstCompanyListingsRepository(mockRemoteDataSource, spyDao)
     }
 
 
     @Test
     fun `test getAllCompanyListings return data`() = testScope.runTest {
-        val expected = listOf(
-            CompanyListings(1, "A", "Agilent Technologies Inc", "NYSE"),
-            CompanyListings(2, "AA", "Alcoa Corp", "NYSE"),
-        )
-
-        every { mockDao.getCompanyListings() } returns flowOf(
-            listOf(
-                CompanyListingEntity(1, "A", "Agilent Technologies Inc", "NYSE"),
-                CompanyListingEntity(2, "AA", "Alcoa Corp", "NYSE"),
-            )
-        )
+        companyListingsDao.insertOrIgnoreCompanyListings(testEntityData)
 
         val actual = repository.getCompanyListings().first()
-        verify(exactly = 1) { mockDao.getCompanyListings() }
-        assertEquals(expected, actual)
 
+        verify(exactly = 1) { spyDao.getCompanyListings() }
+        assertEquals(testEntityData.toListExternalModel(), actual)
     }
 
     @Test
     fun `test getAllCompanyListings by query return data`() = testScope.runTest {
-        val query = "aa"
-        val expected = listOf(CompanyListings(2, "AA", "Alcoa Corp", "NYSE"))
+        companyListingsDao.insertOrIgnoreCompanyListings(testEntityData)
 
-        every { mockDao.getCompanyListings(query) } returns flowOf(
-            listOf(
-                CompanyListingEntity(
-                    2,
-                    "AA",
-                    "Alcoa Corp",
-                    "NYSE"
-                )
-            )
-        )
+        val query = "Agilent"
+        val expected = listOf(CompanyListings("A", "Agilent Technologies Inc", "NYSE"))
 
         val actual = repository.getCompanyListings(query).first()
-        verify(exactly = 1) { mockDao.getCompanyListings(query) }
+        verify(exactly = 1) { spyDao.getCompanyListings(query) }
         assertEquals(expected, actual)
     }
 
 
     @Test
-    fun `test databaseSync, verify fetch data from internet, clear current data and insert new data to the database`() =
+    fun `test syncWith, verify fetch data from internet and insert new data to the database`() =
         testScope.runTest {
-            val fakeCompanyListingsDao = FakeCompanyListingsDao()
-            val repo =
-                OfflineFirstCompanyListingsRepository(mockRemoteDataSource, fakeCompanyListingsDao)
             val noopSynchronizer = object : Synchronizer {}
-            val remoteData = listOf(
+            val testData = listOf(
                 CompanyListingDto("AZA", "Foo tch", "NYE"),
                 CompanyListingDto("AKA", "Thursday Corp", "NYSE"),
             )
 
+            coEvery { mockRemoteDataSource.getCompanyListing() } returns testData
+
+            repository.syncWith(noopSynchronizer)
+            coVerify(exactly = 1) { mockRemoteDataSource.getCompanyListing() }
+
+            val actual = companyListingsDao.getCompanyListings().first()
             val expected = listOf(
-                CompanyListingEntity(0, "AZA", "Foo tch", "NYE"),
-                CompanyListingEntity(0, "AKA", "Thursday Corp", "NYSE"),
+                CompanyListingEntity("AZA", "Foo tch", "NYE"),
+                CompanyListingEntity("AKA", "Thursday Corp", "NYSE"),
             )
-
-            coEvery { mockRemoteDataSource.getCompanyListing() } returns remoteData
-
-            repo.syncWith(noopSynchronizer)
-            coVerify { mockRemoteDataSource.getCompanyListing() }
-
-            val actual = fakeCompanyListingsDao.getCompanyListings().first()
 
             assertEquals(expected, actual)
         }
 
 }
+
+private val testEntityData = listOf(
+    CompanyListingEntity("A", "Agilent Technologies Inc", "NYSE"),
+    CompanyListingEntity("AA", "Alcoa Corp", "NYSE"),
+    CompanyListingEntity("AAA", "AAX FIRST PRIORITY CLO BOND ETF", "NYSE ARCA"),
+    CompanyListingEntity("AEAE", "AltEnergy Acquisition Corp - Class A", "NASDAQ"),
+    CompanyListingEntity("ADXN", "Addex Therapeutics Ltd", "NASDAQ"),
+    CompanyListingEntity("AA", "Aether Ltd", "NASDAQ"),
+)
