117ff6dc5082d7024688b8146329e78c5dcc96e0
muhammad rio <muhammad.rioperm@gmail.com>
2023-06-03 18:41:49 +0700

    Provides ResourceHelper dependency injection

--- core/common/src/main/java/com/riopermana/common/di/CommonModule.kt
+++ core/common/src/main/java/com/riopermana/common/di/CommonModule.kt
@@ -1,10 +1,14 @@
 package com.riopermana.common.di
 
+import android.content.Context
 import com.riopermana.common.csv.CsvParser
 import com.riopermana.common.csv.OpenCsvParser
+import com.riopermana.common.helper.StringResHelper
+import com.riopermana.common.helper.ResourcesHelper
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -15,4 +19,9 @@ class CommonModule {
     @Provides
     @Singleton
     fun providesCsvParser() : CsvParser = OpenCsvParser()
+
+    @Provides
+    fun providesResourceHelper(
+        @ApplicationContext context: Context
+    ) : ResourcesHelper<String> = StringResHelper(context)
 }
