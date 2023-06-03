a454fe867b401c458c85967a2d6d1aea72b9e5f0
muhammad rio <muhammad.rioperm@gmail.com>
2023-06-03 11:27:28 +0700

    Remove property "id" from CompanyListingEntity, CompanyListingEntity now uses "name", "symbol", and "exchange" properties as it's primary keys.

--- core/database/src/main/java/com/riopermana/database/entities/CompanyListingEntity.kt
+++ core/database/src/main/java/com/riopermana/database/entities/CompanyListingEntity.kt
@@ -2,22 +2,20 @@ package com.riopermana.database.entities
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 
 /**
  * Database entity representing a company listing in the local database.
  * It contains properties for the [symbol], [name], and [exchange] of the company.
  */
 @Entity(
-    tableName = "company_listings"
+    tableName = "company_listings",
+    primaryKeys = ["name", "symbol", "exchange"]
 )
 data class CompanyListingEntity(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int = 0,
     @field:ColumnInfo("symbol")
-    val symbol:String? = null,
+    val symbol:String,
     @field:ColumnInfo("name")
-    val name:String? = null,
+    val name:String,
     @field:ColumnInfo("exchange")
-    val exchange:String? = null,
+    val exchange:String
 )
